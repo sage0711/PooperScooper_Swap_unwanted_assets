21a14b4b4441db8fee0a1d4f2163cb8769e1792b
jermaine06_colevo <jermaine06_colevo@outlook.com>
2024-06-28 15:30:33 -0400

    add: vite config upload

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+    },
+  },
+  define: {
+    Buffer: ["buffer", "Buffer"],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+});
